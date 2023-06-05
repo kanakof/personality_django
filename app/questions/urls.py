f039104c1c4c7a711ecfa818616f21efd093fa6d
kanakof <kanakofukuda199531@gmail.com>
2023-06-05 23:48:37 +0900

    feat:urls＆viewsファイル構築(wip)

--- app/questions/urls.py
+++ app/questions/urls.py
@@ -3,5 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.index, name="index"),
+    path("", views.front, name="front"),
+    # path("", views.index, name="index")
+
 ]
--- app/questions/views.py
+++ app/questions/views.py
@@ -1,6 +1,18 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+def front(request):
+    return render(request, "questions/front.html")
+
 def index(request):
     # return HttpResponse("Hello, world. You're at the polls index.")
     return render(request, "questions/index.html")
+
+def result_a(request):
+    return render(request, "questions/result_a.html")
+
+def result_b(request):
+    return render(request, "questions/result_b.html")
+
+def result_c(request):
+    return render(request, "questions/result_c.html")
