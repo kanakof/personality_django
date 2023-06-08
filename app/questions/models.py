e11cb59066c04be810968348d10ace439ca222b8
kanakof <kanakofukuda199531@gmail.com>
2023-06-08 12:04:34 +0900

    feat: model.py追記 データベース構築

--- app/questions/models.py
+++ app/questions/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
-# Create your models here.
+# # Create your models here.
+
+GENDER_CHOICES = (
+   ('M', 'Male'),
+   ('F', 'Female'),
+   ('N', 'Noanswer'),
+)
+
+BLOODTYPE_CHOICES = (
+    ('A', 'A型'),
+    ('B', 'B型'),
+    ('O', 'O型'),
+    ('AB', 'AB型'),
+)
+
+
+class Questions(models.Model):
+    name = models.CharField(max_length=100)
+    gender = models.ChoiceField(choices=GENDER_CHOICES, max_length=1)
+    bloodtype = models.ChoiceField(choices=BLOODTYPE_CHOICES, max_length=1)
