ad6d729bbb7d3557ef5d7e4064ad2c57881118fa
kanakof <kanakofukuda199531@gmail.com>
2023-06-11 00:53:18 +0900

    feat: forms.py 修正

--- app/questions/forms.py
+++ app/questions/forms.py
@@ -1,6 +1,8 @@
 from django import forms
+from .models import Question
 
-# class Person(forms.Form):
-#     first_name = models.CharField(blank=True, max_length=50)
-#     last_name = models.CharField(blank=True, max_length=50)
-#     paid_member = models.BooleanField()
+# class Questionform(forms.Form):
+#     class 
+
+
+
