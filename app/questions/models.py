b1dd0d72db6abd5f33eb08003cf762dd028140c5
DaisukeMiyazaki <aubozumom@icloud.com>
2023-06-11 21:00:12 +0900

    feat: 不要なコードを削除し、質問モデルを定義

--- app/questions/models.py
+++ app/questions/models.py
@@ -1,22 +1,12 @@
 from django.db import models
-
-# # Create your models here.
-
-GENDER_CHOICES = (
-   ('M', 'Male'),
-   ('F', 'Female'),
-   ('N', 'Noanswer'),
-)
-
-BLOODTYPE_CHOICES = (
-    ('A', 'A型'),
-    ('B', 'B型'),
-    ('O', 'O型'),
-    ('AB', 'AB型'),
-)
+from django.utils import timezone
 
 
-class Questions(models.Model):
-    name = models.CharField(max_length=100)
-    gender = models.ChoiceField(choices=GENDER_CHOICES, max_length=1)
-    bloodtype = models.ChoiceField(choices=BLOODTYPE_CHOICES, max_length=1)
+class Question(models.Model):
+    body = models.CharField(max_length=100)
+    created_at = models.DateTimeField(default=timezone.now)
+    updated_at = models.DateTimeField(default=timezone.now)
+
+    class Meta:
+        verbose_name = "質問"
+        verbose_name_plural = "質問一覧"
