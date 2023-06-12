479beb2350ca52ded721688a6eaf2ce0a2562fbf
DaisukeMiyazaki <aubozumom@icloud.com>
2023-06-12 18:59:50 +0900

    bugfix: とりあえず動くように導線を修正

--- app/questions/views.py
+++ app/questions/views.py
@@ -1,38 +1,34 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views.generic import View
 
 
-def index(request):
-    return render(request, "questions/index.html")
-
-def page(request):
-    return render(request, "questions/page.html")
-
-
 class ProcessView(View):
-    def post(self,request):
+    def get(self, request):
+        return render(request, "questions/question_list.html")
+
+    def post(self, request):
         """POSTの結果次第で、レンダリングする"""
         print("post request is received")
         result_type = self.get_personality_type(request)
-        if result_type == 'type A':
-            return render(request, "questions/result_a.html")
-        elif result_type == 'type B':
-            return render(request, "questions/result_b.html")
-        elif result_type == 'type C':
-            return render(request, "questions/result_c.html")
+        if result_type == "type A":
+            return redirect("result")
+        elif result_type == "type B":
+            return redirect("result")
+        elif result_type == "type C":
+            return redirect("result")
+        return redirect("result")
 
-
-    def get_personality_type(self,request):
+    def get_personality_type(self, request):
         """group1(=タイプA),group2(=タイプB),group3(=タイプC)"""
         """[]内の数値はhtmlのitem名"""
-        group1 = ["1","4","8","10","13","17"]
-        group2 = ["2","6","9","12","15","18"]
-        group3 = ["3","5","7","11","14","16"]
+        group1 = ["1", "4", "8", "10", "13", "17"]
+        group2 = ["2", "6", "9", "12", "15", "18"]
+        group3 = ["3", "5", "7", "11", "14", "16"]
 
         """group1(=タイプA)の合計を算出"""
         total = 0
         for i in group1:
-            point_str = request.POST.get('item{}'.format(i))
+            point_str = request.POST.get("item{}".format(i))
             print(point_str)
             if point_str:
                 total += int(point_str)
@@ -41,7 +37,7 @@ class ProcessView(View):
         """group2(=タイプB)の合計を算出"""
         total2 = 0
         for i in group2:
-            point_str = request.POST.get('item{}'.format(i))
+            point_str = request.POST.get("item{}".format(i))
             print(point_str)
             if point_str:
                 total2 += int(point_str)
@@ -50,7 +46,7 @@ class ProcessView(View):
         """group3(=タイプC)の合計を算出"""
         total3 = 0
         for i in group3:
-            point_str = request.POST.get('item{}'.format(i))
+            point_str = request.POST.get("item{}".format(i))
             print(point_str)
             if point_str:
                 total3 += int(point_str)
@@ -71,7 +67,15 @@ class ProcessView(View):
             personality_type = "C"
         elif total == total2 > total3 or total == total3 > total2:
             personality_type = "A"
-        elif total2 == total3 >total:
+        elif total2 == total3 > total:
             personality_type = "C"
         personality_type = "type " + personality_type
         return personality_type
+
+
+class ResultView(View):
+    def get(self, request):
+        redirect_template = "questions/result_a.html"
+        redirect_template = "questions/result_b.html"
+        redirect_template = "questions/result_c.html"
+        return render(request, redirect_template)
