99725b6d78fb762fa6be499c8b936898fb405ab3
kanakof <kanakofukuda199531@gmail.com>
2023-07-02 23:13:12 +0900

    feat:adminにUserを追加

--- app/questions/admin.py
+++ app/questions/admin.py
@@ -2,7 +2,8 @@ from django.contrib import admin
 
 # Register your models here.
 
-from .models import Question, Answer
+from .models import Question, Answer, User
 
 admin.site.register(Question)
 admin.site.register(Answer)
+admin.site.register(User)
