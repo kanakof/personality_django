7f4dc0c2e85d8bf5999d17b8f09986f61b415a27
kanakof <kanakofukuda199531@gmail.com>
2023-07-04 18:29:48 +0900

    feat:models.pyを最新verに更新

--- app/questions/models.py
+++ app/questions/models.py
@@ -1,27 +1,64 @@
 from django.db import models
 from django.utils import timezone
 
+
 class Question(models.Model):
-    body = models.CharField(max_length=100)
+    body = models.CharField(max_length=100, default="")
     created_at = models.DateTimeField(default=timezone.now)
     updated_at = models.DateTimeField(default=timezone.now)
+
+    def __str__(self):
+        return self.body
 
     class Meta:
         verbose_name = "質問"
         verbose_name_plural = "質問一覧"
 
 
+class GenderChoices(models.IntegerChoices):
+    FEMALE = 1, "女性"
+    MALE = 2, "男性"
+    NONE = 3, "無回答"
+
+
+class BTypeChoices(models.IntegerChoices):
+    A = 1, "A型"
+    B = 2, "B型"
+    O = 3, "O型"
+    AB = 4, "AB型"
+
+
+class User(models.Model):
+    name = models.CharField(max_length=100, default="")
+    g_choice = models.IntegerField(
+        choices=GenderChoices.choices, default=GenderChoices.NONE
+    )
+    b_choice = models.IntegerField(choices=BTypeChoices.choices, default=BTypeChoices.A)
+    created_at = models.DateTimeField(default=timezone.now)
+    updated_at = models.DateTimeField(default=timezone.now)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = "User情報"
+        verbose_name_plural = "User情報一覧"
+
+
 class AnswerChoices(models.IntegerChoices):
-    STRONGLY_AGREE = 1, "強く同意する"
-    AGREE = 2, "同意する"
+    STRONGLY_AGREE = 1, "とても同意する"
+    AGREE = 2, "まあ同意する"
     NEUTRAL = 3, "どちらでもない"
-    DISAGREE = 4, "同意しない"
-    STRONGLY_DISAGREE = 5, "強く同意しない"
+    DISAGREE = 4, "あまり同意しない"
+    STRONGLY_DISAGREE = 5, "全く同意しない"
 
 
 class Answer(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, default="")
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
-    choice = models.IntegerField(choices=AnswerChoices.choices, default=AnswerChoices.NEUTRAL)
+    choice = models.IntegerField(
+        choices=AnswerChoices.choices, default=AnswerChoices.NEUTRAL
+    )
     created_at = models.DateTimeField(default=timezone.now)
     updated_at = models.DateTimeField(default=timezone.now)
 
