b000d3c71c491b8279ddee9670a0b5233e6fb400
kanakof <kanakofukuda199531@gmail.com>
2023-07-06 00:17:48 +0900

    今回は使わなかったforms.py（記録用です）

---
+++ app/questions/forms.py
@@ -0,0 +1,13 @@
+# from django import forms
+# from django.forms import ModelForm
+# from .models import Answer, User
+
+
+# class UserForm(ModelForm):
+#     class Meta:
+#         model = User
+#         fields = ("user", "question", "choice", "created_at", "updated_at")
+
+
+
+
